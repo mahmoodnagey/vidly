cd020c85b7c5487cae732560a2c81d2010d87467
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2023-05-14 18:10:30 +0300

    Build the customers API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/vidly')
     
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
-app.listen(3000, () => console.log(`Listening on ${port}...`));
+app.listen(port, () => console.log(`Listening on ${port}...`));
