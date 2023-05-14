259a4d3e4fa6eb03affa86f07b666ec7c5507612
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2023-05-14 16:28:52 +0300

    first test for mongoose with express

---
+++ index.js
@@ -0,0 +1,15 @@
+const genres = require('./routes/genres');
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/vidly')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+    
+app.use(express.json());
+app.use('/api/genres', genres);
+
+const port = process.env.PORT || 3000;
+app.listen(3000, () => console.log(`Listening on ${port}...`));
