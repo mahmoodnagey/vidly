9d59f5d72c06ce151776c0c15d23642bc9fb4f32
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2023-05-16 13:05:26 +0300

    Build Movies API and Restructuring Project files

---
+++ models/genre.js
@@ -0,0 +1,25 @@
+const Joi = require('joi'); // if you had a problem, confirm you use 13.1.0 version of joi
+const mongoose = require('mongoose');
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
+
+const Genre = mongoose.model('Genre', genreSchema);
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+exports.genreSchema = genreSchema;
+exports.Genre = Genre; 
+exports.validate = validateGenre;
